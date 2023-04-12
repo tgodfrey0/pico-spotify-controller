9520f626700c8d71214d4b9391c882c21a8afdd5
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-04-12 21:08:33 +0100

    backend done?

--- src/spotify.c
+++ src/spotify.c
@@ -18,7 +18,7 @@ volatile bool playing = false;
 bool initialised = false;
 
 void spotify_init(){
-  if(initialised) return;
+  if(!initialised) return;
 
   // Init lwjson
   lwjson_init(&lwjson, tokens, LWJSON_ARRAYSIZE(tokens));
@@ -26,23 +26,61 @@ void spotify_init(){
   tls_client_send_data(cmd_get_playback);
 }
 
-void togglePlayback(){}
-void play(){}
-void pause(){}
-void next(){}
-void previous(){}
+void sync_playback(){
+  if(!initialised) return;
+  tls_client_send_data(cmd_get_playback);
+}
+
+void togglePlayback(){
+  if(!initialised) return;
+  if(playing){
+    pause();
+  } else {
+    play();
+  }
+}
+
+void play(){
+  if(!initialised) return;
+  tls_client_send_data(cmd_play);
+}
+
+void pause(){
+  if(!initialised) return;
+  tls_client_send_data(cmd_pause);
+}
+
+void next(){
+  if(!initialised) return;
+  tls_client_send_data(cmd_next);
+}
+
+void previous(){
+  if(!initialised) return;
+  tls_client_send_data(cmd_previous);
+}
 
 void parse_response(void *arg){
   char *res = (char*)arg;
 
   if (lwjson_parse(&lwjson, res) == lwjsonOK) {
     const lwjson_token_t* t;
-    printf("JSON parsed..\r\n");
 
     /* Find custom key in JSON */
-    if ((t = lwjson_find(&lwjson, "mykey")) != NULL) {
-      printf("Key %s found with data type: %d\r\n", t->token_value, (int)t->type);
+    if ((t = lwjson_find(&lwjson, "is_playing")) != NULL) {
+      printf("Token found\n");
+      if(t->type == LWJSON_TYPE_TRUE){
+        playing = true;
+      } else if(t->type == LWJSON_TYPE_FALSE){
+        playing = false;
+      } else {
+        printf("Invalid is_playing value\n");
+      }
+    } else {
+      printf("Token not found!\n");
     }
 
+  } else {
+    printf("Failed to parse response\n");
   }
 }
--- src/spotify.h
+++ src/spotify.h
@@ -1,9 +1,12 @@
 #ifndef SPOTIFY_H
 #define SPOTIFY_H
 
+#include "lwjson/lwjson.h"
+
 void parse_response(void *arg);
 
 void spotify_init(void);
+void sync_playback(void);
 void togglePlayback(void);
 void play(void);
 void pause(void);
