8e4799b399b13828b52a5f6bcee65e6216d09ef4
Toby Godfrey <toby.godfrey2003@gmail.com>
2023-04-16 20:06:51 +0100

    defs

--- src/defs.h
+++ src/defs.h
@@ -4,21 +4,7 @@
 #include "pico/stdlib.h"
 #include <stdint.h>
 
-#define TLS_CLIENT_HTTP_REQUEST  "GET /api/ip HTTP/1.1\r\n" \
-	"Host: " TLS_CLIENT_server "\r\n" \
-	"Connection: close\r\n" \
-	"\r\n"
 #define TLS_CLIENT_TIMEOUT_SECS  15
-
-// Constants
-#define SWITCH_A_PLAYBACK     16
-#define SWITCH_B              17
-#define SWITCH_X_NEXT         19
-#define SWITCH_Y_PREVIOUS     20
-
-#define LED_R                 9
-#define LED_G                 10
-#define LED_B                 11
 
 // Request Strings
 const char* const server = "api.spotify.com";
